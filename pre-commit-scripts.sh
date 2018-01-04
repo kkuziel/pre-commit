984ffae53c8f725d7cf18f2c03060285b33b0ae3
Krzysztof Kuziel <krzysztof.kuziel@performgroup.com>
2018-01-04 17:35:38 +0100

    a

--- pre-commit-scripts.sh
+++ pre-commit-scripts.sh
@@ -4,7 +4,7 @@ console.log('\x1b[1m','Please wait for tests to run...','\x1b[0m');
 console.log('You can','\x1b[1m','skip','\x1b[0m','the precommit hook by running "git commit -n" instead','\x1b[1m', "but beware!",'\x1b[0m');
 
 require('child_process').exec(
-  'npm test',
+  'npm run test',
   function (error, stdout) {
     console.log(stdout && 'stdout: ' + stdout);
     if (error !== null) {
