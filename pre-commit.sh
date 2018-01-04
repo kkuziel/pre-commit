2cd1d50e2cb6483f368a4cbb58dd4e7ce1b767c2
Krzysztof Kuziel <krzysztof.kuziel@performgroup.com>
2018-01-04 18:19:06 +0100

    test

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-echo "Runnint tests..."
-npm run test
+echo "Please wait for tests to run..."
+echo "You can skip the precommit hook by running git commit -n instead but dont do that"
+npm run testFail
