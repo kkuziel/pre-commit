54d347254492cb5a32e66f1c3f4ca386ec50b893
Krzysztof Kuziel <krzysztof.kuziel@performgroup.com>
2018-01-04 18:14:48 +0100

    test

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
-
-# test only commited changes
-STASH_NAME="pre-commit-$(date +%s)"
-git stash save -q --keep-index $STASH_NAME
-
 echo "Runnint tests..."
 npm run test
-
-# pop out stash
-STASHES=$(git stash list)
-if [[ $STASHES == "$STASH_NAME" ]]; then
-  git stash pop -q
-fi
